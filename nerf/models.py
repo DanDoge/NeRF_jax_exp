bfeefd4d1fb58145d40c0b9592c81dd604c184f8
huangdaoji <huangdaoji@bytedance.com>
2021-02-22 11:42:03 +0800

    singleloss

--- nerf/models.py
+++ nerf/models.py
@@ -125,7 +125,7 @@ class NerfModel(nn.Module):
       z_vals_mid = .5 * (z_vals[Ellipsis, 1:] + z_vals[Ellipsis, :-1])
       key, rng_1 = random.split(rng_1)
       # nocoarse or sample near depth
-      '''
+      
       z_vals_fine, samples = model_utils.sample_pdf_nocoarse(
           key,
           z_vals_mid,
@@ -136,10 +136,11 @@ class NerfModel(nn.Module):
           num_fine_samples + num_coarse_samples,
           randomized,
       )
-      '''
+      
       mu = (weights * z_vals).sum(axis=-1) / (weights.sum(axis=-1) + 1e-5)
       # zeroout coarse estimation near depth
-      sigma_coarse = jnp.where(((z_vals_coarse > mu[Ellipsis, None] - 0.1) & (z_vals_coarse < mu[Ellipsis, None] + 0.1))[Ellipsis, None], 0., sigma_coarse)
+      sigma_coarse = jnp.where(((z_vals_coarse > mu[Ellipsis, None] - 0.01) & (z_vals_coarse < mu[Ellipsis, None] + 0.01))[Ellipsis, None], 0., sigma_coarse)
+      '''
       z_vals_fine, samples = model_utils.sample_near_depth(
           key, 
           num_fine_samples + num_coarse_samples, 
@@ -151,6 +152,7 @@ class NerfModel(nn.Module):
           origins, 
           directions
       )
+      '''
       samples_enc = model_utils.posenc(samples, 0, deg_point, 1, legacy_posenc_order)
       if use_viewdirs:
         raw_rgb, raw_sigma, _ = mlp_fn(samples_enc, viewdirs_enc)
