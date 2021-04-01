cef40f9111f6b1f87a4334db43e79ab15ed09c3a
huangdaoji <huangdaoji@bytedance.com>
2021-04-01 14:24:21 +0800

    b4c3f9, h256

--- nerf/models.py
+++ nerf/models.py
@@ -104,7 +104,7 @@ class NerfModel(nn.Module):
         net_activation=self.net_activation,
         skip_layer=self.skip_layer)
     mlp_fine = model_utils.MLP_head(
-        net_depth=self.net_depth,
+        net_depth=self.net_depth // 2 * 3,
         net_width=self.net_width,
         net_depth_condition=self.net_depth_condition,
         net_width_condition=self.net_width_condition,
