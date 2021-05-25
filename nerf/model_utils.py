e292cb091b2a2154a5aea14c3ef31e260007898a
huangdaoji <huangdaoji@bytedance.com>
2021-05-25 16:44:31 +0800

    hard

--- nerf/model_utils.py
+++ nerf/model_utils.py
@@ -141,11 +141,19 @@ class full_MLP(nn.Module):
 
     key, rng = jax.random.split(rng)
     gumbel = jax.random.gumbel(key, shape=[self.num_small_nerf])
+
     if it is None:
       prob = jnp.exp((jnp.log(prob + 1e-5) + gumbel) / 0.5)
     else:
-      prob = jnp.exp((jnp.log(prob + 1e-5) + gumbel) / jnp.max(0.5, 1. - it.reshape(-1)[0] / 500000))
+      prob = jnp.exp((jnp.log(prob + 1e-5) + gumbel) / jnp.maximum(0.5, 1. - it.reshape(-1)[0] / 500000))
+
     prob = prob / prob.sum(axis=-1)[Ellipsis, None]
+
+    hard_idx = prob.argmax(-1)
+    hard_prob = jax.nn.one_hot(hard_idx, prob.shape[-1])
+
+    prob = jax.lax.stop_gradient(hard_prob - prob) + prob 
+
     
     rgb = (jnp.stack(list_rgb, axis=-1) * prob[Ellipsis, None, :]).sum(axis=-1)
     sigma = (jnp.stack(list_sigma, axis=-1) * prob[Ellipsis, None, :]).sum(axis=-1)
